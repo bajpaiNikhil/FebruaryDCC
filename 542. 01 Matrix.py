2ccd02258c094ebabbd8d91617e5df6acccb656e
NikhilBajpai <nikhil.cse16@gmail.com>
2022-02-02 18:52:28 +0530

    day 3

--- 542. 01 Matrix.py
+++ 542. 01 Matrix.py
@@ -1,28 +1,26 @@
-mat = [[0, 0, 0], [0, 1, 0], [0, 0, 0]]
+mat = [[0, 0, 0], [0, 1, 0], [1, 1, 1]]
 
 res = []
 
 
-def exploreMatrix(mat, r, c, destination):
-    rowInbound = 0<= r < len(mat)
-    colInbound = 0<= c < len(mat[0])
-
-    if not rowInbound or colInbound :
-        return False
-    queue = [[ r,c, 0]]
-
-    while len(queue)>0:
-        top = queue.pop(0)
-
-        if top[0] == destination:
-            return top[1]
-        exploreMatrix(mat , r-1 ,c , destination)
-        exploreMatrix(mat , r+1 , c ,destination)
-        exploreMatrix(mat , r , c-1 , destination)
-        exploreMatrix(mat , r , c+1 , destination)
+def exploreMat(mat, queue):
+    for row, column in queue:
+        for dx, dy in (1, 0), (-1, 0), (0, 1), (0, -1):
+            newR = row + dx
+            newC = column + dy
+            if 0 <= newR < len(mat) and 0 <= newC < len(mat[0]) and mat[newR][newC] == -1:
+                mat[newR][newC] = mat[row][column] + 1
+                queue.append((newR ,newC))
+    return mat
 
 
+queue = []
 for i in range(len(mat)):
     for j in range(len(mat[0])):
-        exploreMatrix(mat , i ,j , destination = 0)
-
+        if mat[i][j] == 0:
+            queue.append((i, j))
+        else:
+            mat[i][j] = -1
+print(queue)
+exploreMat(mat, queue)
+print(mat)
