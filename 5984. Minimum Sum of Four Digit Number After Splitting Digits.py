d0be34f5be29379b1f9757a1ab9b27f82a13979c
NikhilBajpai <nikhil.cse16@gmail.com>
2022-02-06 13:22:09 +0530

    day 4

---
+++ 5984. Minimum Sum of Four Digit Number After Splitting Digits.py
@@ -0,0 +1,10 @@
+
+
+
+num = 4009
+a = list(str(num))
+nums = a.sort()
+print(nums , a)
+sum1= a[0]+a[-2]
+sum2 = a[1]+a[-1]
+print(sum2 , sum1 , int(sum1)+int(sum2))
---
+++ 5985. Partition Array According to Given Pivot.py
@@ -0,0 +1,14 @@
+nums = [9, 12, 5, 10, 14, 3, 10]
+pivot = 10
+less = []
+equal = []
+greater = []
+
+for i in nums:
+    if i >pivot:
+        greater.append(i)
+    elif i<pivot:
+        less.append(i)
+    else:
+        equal.append(i)
+print(less+equal+greater)
